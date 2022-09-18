35a3f09b590f4f43ac6d4a890f8d68a4eb0ea9fd
Agustin Carrasco <asermax@gmail.com>
2022-09-17 23:36:52 -0300

    Add count to books API

--- pages/api/books.js
+++ pages/api/books.js
@@ -4,6 +4,7 @@ const prisma = new PrismaClient()
 
 export default async function handler(req, res) {
   const { skip, take } = req.query
+  const count = await prisma.book.count();
   const books = await prisma.book.findMany({
     where: {
       userId: 1,
@@ -11,5 +12,6 @@ export default async function handler(req, res) {
     skip: skip != null ? parseInt(skip, 10) : undefined,
     take: take != null ? parseInt(take, 10) : undefined,
   })
+  res.setHeader('X-Total-Count', count);
   res.status(200).json(books)
 }
